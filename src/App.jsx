61f1fafdc365d45242243857d01cc1d6916885cf
ebnybuniad <ebnybuniad@hmail.com>
2025-04-08 16:45:04 +0600

    load countries data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Suspense } from 'react';
 import './App.css'
+import Countries from './components/Countries/Countries'
+
+const loadCountries = fetch("https://restcountries.com/v3.1/all")
+  .then(res => res.json());
 
 function App() {
+
+  const countriesPromise = loadCountries;
+
   return (
+
     <>
-      
+      <Suspense fallback={<h5>Data Loading...</h5>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
